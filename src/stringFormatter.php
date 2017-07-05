68b05b2335b728ea42b3132ba0def1cc79dfe535
Vincent OFFROY <offroy.vincent@gmail.com>
2017-07-05 11:37:11 +0200

    Class developpement
    add of class string with four methods :
    - concat() which concat two strings
    - toCamelCase which concat two strings in camelCase
    - prefix which concat two strings one before the other with camelCase option
    - suffix which concat two strings one after the other with camelCase option

---
+++ src/stringFormatter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Model;
+
+class StringFormatter
+{
+    /**
+     * @return string
+     */
+    public function concat($firstString, $secondString)
+    {
+        return $firstString.$secondString;
+    }
+
+    /**
+     * @return string
+     */
+    public function toCamelCase($firstString, $secondString)
+    {
+        $secondString[0] = strtoupper($firstString[0])
+        $secondString[0] = strtoupper($secondString[0])
+        return $this->concat($firstString.$secondString);
+    }
+
+    /**
+     * @return string
+     */
+    public function prefix($firstString, $secondString, $toCamelCase = false)
+    {
+        if($toCamelCase){
+          return $this->toCamelCase($secondString.$firstString);
+        }
+        return $secondString.$firstString;
+    }
+
+    /**
+     * @return string
+     */
+    public function suffix($firstString, $secondString, $toCamelCase = false)
+    {
+        if($toCamelCase){
+          return $this->toCamelCase($firstString.$secondString);
+        }
+        return $firstString.$secondString;
+    }
+}
